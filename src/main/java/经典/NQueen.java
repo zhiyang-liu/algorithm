e601dbec4d30c09df98b9ef4dfd26624cfda0b92
liuzhiyang <liuzhiyang@baidu.com>
2020-01-29 20:41:50 +0800

    n皇后修改判断条件

--- src/main/java/经典/NQueen.java
+++ src/main/java/经典/NQueen.java
@@ -9,11 +9,6 @@ import java.util.List;
  * Created by liuzhiyang on 2020/1/28 下午7:41
  */
 public class NQueen {
-    int rows[];
-    // "hill" diagonals
-    int hills[];
-    // "dale" diagonals
-    int dales[];
     int n;
     // output
     List<List<String>> output = new ArrayList();
@@ -21,26 +16,18 @@ public class NQueen {
     int queens[];
 
     public boolean isNotUnderAttack(int row, int col) {
-        int res = rows[col] + hills[row - col + 2 * n] + dales[row + col];
-        return (res == 0) ? true : false;
-    }
-
-    public void placeQueen(int row, int col) {
-        queens[row] = col;
-        rows[col] = 1;
-        hills[row - col + 2 * n] = 1;  // "hill" diagonals
-        dales[row + col] = 1;   //"dale" diagonals
-    }
-
-    public void removeQueen(int row, int col) {
-        queens[row] = 0;
-        rows[col] = 0;
-        hills[row - col + 2 * n] = 0;
-        dales[row + col] = 0;
+        for (int i = 0; i < row; ++i) {
+            if (queens[i] == col) {
+                return false;
+            } else if (Math.abs(row - i) == Math.abs(col - queens[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void addSolution() {
-        List<String> solution = new ArrayList<String>();
+        List<String> solution = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
             int col = queens[i];
             StringBuilder sb = new StringBuilder();
@@ -55,25 +42,23 @@ public class NQueen {
     public void backtrack(int row) {
         for (int col = 0; col < n; col++) {
             if (isNotUnderAttack(row, col)) {
-                placeQueen(row, col);
+                queens[row] = col;
                 // if n queens are already placed
                 if (row + 1 == n) addSolution();
                     // if not proceed to place the rest
                 else backtrack(row + 1);
                 // backtrack
-                removeQueen(row, col);
+                queens[row] = 0;
             }
         }
     }
 
     public List<List<String>> solveNQueens(int n) {
         this.n = n;
-        rows = new int[n];
-        hills = new int[4 * n - 1];
-        dales = new int[2 * n - 1];
         queens = new int[n];
 
         backtrack(0);
+
         return output;
     }
 
@@ -81,6 +66,7 @@ public class NQueen {
         NQueen nQueen = new NQueen();
         nQueen.solveNQueens(8);
         System.out.println(Arrays.toString(nQueen.output.toArray()));
+        System.out.println(nQueen.output.size());
     }
 
 }
