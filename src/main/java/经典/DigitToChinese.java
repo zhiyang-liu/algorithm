2c4cff70a37addde2315741a70bb5bcb386c0a41
liuzhiyang <liuzhiyang@baidu.com>
2021-03-01 22:12:00 +0800

    digittostring

--- src/main/java/经典/DigitToChinese.java
+++ src/main/java/经典/DigitToChinese.java
@@ -5,24 +5,34 @@ package 经典;
  */
 public class DigitToChinese {
 
-    private static String toChinese(String str) {
-        String[] s1 = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
-        String[] s2 = { "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千" };
+    public String convert(int number) {
+        // 数字对应的汉字
+        String[] num = {"零", "一", "二", "三","四","五","六","七","八","九"};
+        // 单位
+        String[] unit = {"","十","百","千","万","十","百","千","亿","十","百","千","万亿"};
+
+        // 将输入数字转换为字符串
+        String numString = String.valueOf(number);
+        // 将该字符串分割为数组存放
+        char[] ch = numString.toCharArray();
+
+        // 结果字符串
         String result = "";
-        int n = str.length();
-        for (int i = 0; i < n; i++) {
-            int num = str.charAt(i) - '0';
-            if (i != n - 1 && num != 0) {
-                result += s1[num] + s2[n - 2 - i];
+        int length = ch.length;
+        for (int i = 0; i < length; i++) {
+            int c = ch[i] - '0';
+            if (c != 0) {
+                result += num[c] + unit[length - i - 1];
             } else {
-                result += s1[num];
+                result += num[c];
             }
         }
+
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println("toChinese："+toChinese("1230456"));
+        System.out.println(new DigitToChinese().convert(10109));
     }
 
 }
